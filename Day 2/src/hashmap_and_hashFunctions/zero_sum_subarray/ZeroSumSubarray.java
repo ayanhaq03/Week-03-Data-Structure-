b5905043a03a6e32d82631cc0c20ba198d453397
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-30 12:16:52 +0530

    zero sum subarray problem added

---
+++ Day 2/src/hashmap_and_hashFunctions/zero_sum_subarray/ZeroSumSubarray.java
@@ -0,0 +1,37 @@
+package hashmap_and_hashFunctions.zero_sum_subarray;
+
+
+import java.util.*;
+
+class ZeroSumSubarray {
+    public static List<int[]> findZeroSumSubarrays(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<int[]> result = new ArrayList<>();
+        int sum = 0;
+
+        map.put(0, new ArrayList<>(Arrays.asList(-1))); // To handle prefix sum == 0 case
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum)) {
+                for (int start : map.get(sum)) {
+                    result.add(new int[]{start + 1, i});
+                }
+            }
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+        List<int[]> subarrays = findZeroSumSubarrays(arr);
+
+        for (int[] sub : subarrays) {
+            System.out.println("Subarray: [" + sub[0] + ", " + sub[1] + "]");
+        }
+    }
+}
