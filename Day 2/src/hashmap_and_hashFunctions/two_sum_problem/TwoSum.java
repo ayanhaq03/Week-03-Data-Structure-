47a290053db13abf497c9c51db1bf5b31762318b
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-30 12:18:50 +0530

    two sum problem added

---
+++ Day 2/src/hashmap_and_hashFunctions/two_sum_problem/TwoSum.java
@@ -0,0 +1,28 @@
+package hashmap_and_hashFunctions.two_sum_problem;
+
+import java.util.*;
+
+class TwoSum {
+    public static int[] findTwoSum(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(arr[i], i);
+        }
+
+        return new int[]{-1, -1}; // No solution
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = findTwoSum(arr, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]); // Output: 0, 1
+    }
+}
