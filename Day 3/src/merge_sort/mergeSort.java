33dcada1656a810255b21ff10452a396ca732ca9
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-02 18:22:26 +0530

    merge sort program added

---
+++ Day 3/src/merge_sort/mergeSort.java
@@ -0,0 +1,55 @@
+package merge_sort;
+
+
+
+import java.util.Arrays;
+
+public class mergeSort {
+    // Function to perform Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2; // Find the middle index
+
+            // Recursively sort both halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Function to merge two sorted halves
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        // Merge the arrays back into the original array
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        // Copy any remaining elements
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    public static void main(String[] args) {
+        int[] bookPrices = {500, 200, 700, 100, 300};
+        mergeSort(bookPrices, 0, bookPrices.length - 1);
+        System.out.println("Sorted Book Prices: " + Arrays.toString(bookPrices));
+    }
+}
+
